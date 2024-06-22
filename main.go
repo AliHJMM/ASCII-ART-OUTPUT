1aa258a69b7bd2947cb08e3db336e436690320e9
alihasan6 <AliHasanJMM@gmail.com>
2024-06-22 13:27:55 +0000

    Added ASCII OUTPUT  Function

--- main.go
+++ main.go
@@ -61,3 +61,49 @@ func charValidation(str string) bool {
 	}
 	return true
 }
+
+func ascii_output(args []string) {
+	if len(args) != 4 {
+		fmt.Println("Error : invalid arguments")
+		os.Exit(1)
+	}
+	txt := args[2]
+
+	// fileName:= flag.String("output",)
+	format := args[3]
+	str := ""
+	outputPtr := flag.String("output", "", "Output file name")
+	flag.Parse()
+
+	if *outputPtr == "" {
+		fmt.Println("Usage: go run . --output=<filename>")
+		os.Exit(1)
+	}
+
+	textSlice := strings.Split(txt, "\\n")
+
+	if !charValidation(txt) {
+		fmt.Println("Error : invalid char")
+		os.Exit(1)
+	}
+	file, err := os.ReadFile(format + ".txt")
+	if err != nil {
+		fmt.Println("Error : reading file")
+		os.Exit(1)
+	}
+	slice := strings.Split(string(file), "\n")
+	for j, txt := range textSlice {
+		if txt != "" {
+			for i := 0; i < 8; i++ {
+				for _, v := range txt {
+					firstLine := int(v-32)*9 + 1 + i
+					str += slice[firstLine]
+				}
+				str += "\n"
+			}
+		} else if j != len(textSlice)-1 {
+			str += "\n"
+		}
+	}
+	os.WriteFile(*outputPtr, []byte(str), 0o644)
+}
